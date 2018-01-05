751b59e163762ae7b741a1a60adc55ad4e5952ee
nomnux <3482435+nomnux@users.noreply.github.com>
2018-01-05 21:41:59 +0900

    Update post_build script

--- script/post_build_notification.sh
+++ script/post_build_notification.sh
@@ -5,6 +5,6 @@ set -eux
 aws sns publish \
     --topic-arn "${SNS_TOPIC_ARN}" \
     --subject "AWS CodeBuild - ${CODEBUILD_BUILD_ID}" \
-    --message "Build Completed. ${IMAGE_REPO_NAME_WEBAPP}:${IMAGE_TAG}"
+    --message "Build Completed. Imange:${IMAGE_REPO_NAME_WEBAPP} Initiator:${CODEBUILD_INITIATOR}"
 
 exit 0
